9488ce65a6046151e354e57b0158b49972241e7a
Hiram Torres <is700845@iteso.mx>
2018-08-27 08:46:11 -0500

    Added Tortilla test

---
+++ src/test/java/mx/iteso/ut/TortillaMaizTest.java
@@ -0,0 +1,38 @@
+package mx.iteso.ut;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class TortillaMaizTest {
+    TortillaMaiz tortillaMaiz;
+
+    @Before
+    public void setUp(){
+        tortillaMaiz = new TortillaMaiz();
+    }
+
+    @Test
+    public void testCurrentTemperature(){
+        tortillaMaiz.setCurrentTemperature(21);
+        assertEquals(21, tortillaMaiz.getCurrentTemperature());
+    }
+
+    @Test
+    public void testFalseToast(){
+        tortillaMaiz.toast(false);
+        assertFalse(tortillaMaiz.isToasted());
+    }
+
+    @Test
+    public void testTrueToast(){
+        tortillaMaiz.toast(true);
+        assertTrue(tortillaMaiz.isToasted());
+    }
+    
+    @Test
+    public void testToasting(){
+        assertEquals(20, tortillaMaiz.getToastTemperature());
+    }
+
+}
