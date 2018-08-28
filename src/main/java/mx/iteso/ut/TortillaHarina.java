3d72800aade5dc1e7cc384646c763a53e300a72b
ManuelSanchez25 <39495445+ManuelSanchez25@users.noreply.github.com>
2018-08-27 22:24:25 -0500

    Add files via upload
    Añadir pruebas (test)

--- src/main/java/mx/iteso/ut/TortillaHarina.java
+++ src/main/java/mx/iteso/ut/TortillaHarina.java
@@ -1,23 +1,27 @@
 package mx.iteso.ut;
 
 public class TortillaHarina implements Tortilla {
+
+    boolean toasted;
+    int toast = 10;
+    int temperature;
     public boolean isToasted() {
-        return false;
+        return this.toasted;
     }
 
-    public int getToastTemperature() {
-        return 0;
+    public int getToastTemperature() { return this.toast;
     }
 
     public int getCurrentTemperature() {
-        return 0;
+        return this.temperature;
     }
 
     public void setCurrentTemperature(int temp) {
+        this.temperature = temp;
 
     }
 
-    public void toast(boolean toasted) {
+    public void toast(boolean toasted) {this.toasted=toasted;
 
     }
 }
--- src/main/java/mx/iteso/ut/TortillaMaiz.java
+++ src/main/java/mx/iteso/ut/TortillaMaiz.java
@@ -1,23 +1,28 @@
 package mx.iteso.ut;
 
 public class TortillaMaiz implements Tortilla {
+    boolean toasted ;
+    int toast = 20;
+    int temperature;
     public boolean isToasted() {
-        return false;
+        return this.toasted;
     }
 
     public int getToastTemperature() {
-        return 0;
+        return this.toast;
     }
 
     public int getCurrentTemperature() {
-        return 0;
+        return this.temperature;
     }
 
     public void setCurrentTemperature(int temp) {
+        this.temperature = temp;
 
     }
 
     public void toast(boolean toasted) {
+        this.toasted = toasted;
 
     }
 }
