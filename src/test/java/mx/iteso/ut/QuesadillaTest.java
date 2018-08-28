b4b89d9f02e7bc2d2079ff04ed3ceeda47805bec
ManuelSanchez25 <39495445+ManuelSanchez25@users.noreply.github.com>
2018-08-27 22:17:56 -0500

    Add files via upload
    Añadí las pruebas

--- src/test/java/mx/iteso/ut/QuesadillaTest.java
+++ src/test/java/mx/iteso/ut/QuesadillaTest.java
@@ -47,11 +47,28 @@ public class QuesadillaTest
        verify(mockedQueso,times(1)).melt(true);
    }
    @Test
-   public void quesadillaTerrible(){
-       fail("Please implement corresponding test");
+   public void quesadillaTerrible()
+   {
+       when(mockedQueso.isMelted()).thenReturn(false);
+       when(mockedTortilla.isToasted()).thenReturn(true);
+       when(mockedTortilla.getCurrentTemperature()).thenReturn(2,8,8,8,14);
+       when(mockedTortilla.getToastTemperature()).thenReturn(10);
+       when(mockedQueso.getCurrentTemperature()).thenReturn(2,8,8,8,14);
+       when(mockedQueso.getMeltingTemperature()).thenReturn(20);
+       assertEquals("Terrible quesadilla ",quesadilla.prepareSingle());
+       verify(mockedTortilla,times(1)).toast(true);
+       verify(mockedQueso,never()).melt(true);
    }
    @Test
    public void noHayGas(){
-      fail("Please implement corresponding test");
+       when(mockedQueso.isMelted()).thenReturn(false);
+       when(mockedTortilla.isToasted()).thenReturn(false);
+       when(mockedTortilla.getCurrentTemperature()).thenReturn(50);
+       when(mockedTortilla.getToastTemperature()).thenReturn(10);
+       when(mockedQueso.getCurrentTemperature()).thenReturn(50);
+       when(mockedQueso.getMeltingTemperature()).thenReturn(10);
+       assertEquals("You ran out of gas",quesadilla.prepareSingle());
+       verify(mockedTortilla,never()).toast(true);
+       verify(mockedQueso,never()).melt(true);
    }
 }
